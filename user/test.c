b26a23b3f6f8e8b6b209cf79e98d531953089f95
Sean Myers <sean@sean.(none)>
2012-11-09 07:20:15 -0500

    DONEZIE

--- user/test.c
+++ user/test.c
@@ -19,17 +19,29 @@ int main(int argc, char ** argv) {
     real_buf = pet_malloc(10);
 
     real_buf2 = pet_malloc(10);
-    real_buf2[0] = 'c';
-   real_buf[0] = 'b';
-    while(i < 60000){
+    real_buf2[0] = 's';
+    real_buf2[1] = 'u';
+    real_buf2[2] = 'c';
+    real_buf2[3] = 'c';
+    real_buf2[4] = 'e';
+    real_buf2[5] = 's';
+    real_buf2[6] = 's';
+    real_buf2[7] = '\0';
+    real_buf[0] = 't';
+    real_buf[1] = 'e';
+    real_buf[2] = 's';
+    real_buf[3] = 't';
+    real_buf[4] = '\0';
+    while(i < 33000){
 
         buf = pet_malloc(10);
         buf[0] ='a';
         i++;
     }
 
-    printf("%c\n", real_buf[0]);
-
-    printf("%c\n", real_buf2[0]);
+    //Should output b then c
+    printf("%s\n", real_buf);
+    printf("%s\n", real_buf2);
+    printf("Holy hell IT WORKED\n");
     return 0;
 }
